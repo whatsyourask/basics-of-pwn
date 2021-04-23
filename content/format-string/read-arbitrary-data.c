61dff54fa54d1729b83915d9ba5b85df0d1c61ed
whatsyourask <lexeymartynov@gmail.com>
2021-04-23 11:19:33 +0300

    A program to demonstrate reading arbitrary data subsection

---
+++ content/format-string/read-arbitrary-data.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+// gcc read-arbitrary-data.c -o read-arbitrary-data -m32
+
+int main(int argc, char *argv[]){
+  const char *allow = "Access granted.\n";
+  const char *deny = "Access denied.\n";
+  char buff[200];
+  read(1, buff, 200);
+  if (strncmp("password", buff, 9)) {
+    printf(allow);
+    system("/bin/sh");
+  } else {
+    printf(deny);
+    printf("Say goodbye!!!");
+  }
+  read(1, buff, 200);
+  printf(buff);
+}
