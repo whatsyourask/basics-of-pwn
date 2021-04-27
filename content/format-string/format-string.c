4f70afc531f6858e6554fc347352b022107c2013
whatsyourask <lexeymartynov@gmail.com>
2021-04-27 11:49:10 +0300

    Recompiled it with additional options

--- content/format-string/format-string.c
+++ content/format-string/format-string.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-// gcc format-string.c -o format-string -m32
+// gcc format-string.c -o format-string -fno-stack-protector -no-pie -z execstack -m32
+
 
 int main(int argc, char *argv[]){
   char buff[200];
